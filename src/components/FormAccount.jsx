42da4b331046007e156d49a27fb4b0993d942e46
Igor Gregorio Prati <igorprati@hotmail.com>
2021-09-22 23:52:31 -0300

    form criar conta e cadastro

--- src/components/FormAccount.jsx
+++ src/components/FormAccount.jsx
@@ -1,24 +1,87 @@
-import { Form, Col, Row, FormGroup, Label, Input, FormFeedback, FormText } from 'reactstrap';
-
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import '../styles/formAccount.scss'
+import { Alert } from 'reactstrap';
 
 export default function FormAccount() {
 
+	const [registrar, setRegistrar] = useState(false)
+
 	return (
-		<form>
-			<div className='d-flex flex-column w-50 m-auto'>
-					<Form>
-						<div className='d-flex flex-column mt-2'>
-							<label htmlFor="">Nome de usuário</label>
-							<input type="text" placeholder='Digite seu nome de usuário' />
-						</div>
-						<div className='d-flex flex-column mt-2'>
-							<label htmlFor="">Senha</label>
-							<input type="text" placeholder='Digite sua senha' />
-						</div>
-						<button className='mt-3' type='submit'>Fazer o login</button>
-					</Form>
-					<p className='mt-2'>Ainda não tem uma conta? <a href=''>Crie uma conta.</a> </p>
+		<>
+		{!registrar ? (
+			<form className='col-12 col-lg-8 '>
+			<div className=' d-flex flex-column'>
+				<h3 className='fw-bold'>Login</h3>
+				<p>Entre para finalizar sua compra.</p>
+
+				{/* <Alert color='danger'>Erro ao criar conta</Alert> */}
+
+				<div className='d-flex flex-column mt-1'>
+					<label htmlFor="">Nome de usuário</label>
+					<input className='mt-1' type="text" placeholder='Digite seu nome de usuário' />
+				</div>
+
+				<div className='d-flex flex-column mt-3'>
+					<label htmlFor="">Senha</label>
+					<input className='mt-1' type="password" placeholder='Digite sua senha' />
+				</div>
+
+				<button className='mt-3' type='submit'>Fazer o login</button>
+				<p className='mt-3 mx-auto'>Ainda não tem uma conta? <a onClick={e => setRegistrar(true)}>Crie uma conta.</a> </p>
+
 			</div>
 		</form>
+		) : (
+			<form className='col-12 col-lg-6'>
+			<div className='d-flex flex-column'>
+				<h3 className='fw-bold'>Criar conta</h3>
+				<p>Crie uma conta para prosseguir com sua compra.</p>
+
+				{/* <Alert color='danger'>Erro ao criar conta</Alert> */}
+
+				<div className='d-flex flex-column mt-1'>
+					<label htmlFor="">Nome completo</label>
+					<input className='mt-1' type="text" placeholder='Digite seu nome de usuário' />
+				</div>
+
+				<div className='d-flex flex-column mt-3'>
+					<label htmlFor="">CPF</label>
+					<input className='mt-1' type="text" placeholder='Digite seu cpf' />
+				</div>
+
+				<div className='d-flex flex-column mt-3'>
+					<label htmlFor="">Email</label>
+					<input className='mt-1' type="email" placeholder='O email que você mais usa' />
+				</div>
+
+				<div className='d-flex flex-column mt-3'>
+					<label htmlFor="">Telefone</label>
+					<input className='mt-1' type="text" placeholder='Telefone para contato' />
+				</div>
+
+				<div className='d-flex flex-column mt-3'>
+					<label htmlFor="">Data de nascimento</label>
+					<input className='mt-1' type="date" placeholder='Digite seu nome de usuário' />
+				</div>
+
+				<div className='d-flex flex-column mt-3'>
+					<label htmlFor="">Senha</label>
+					<input className='mt-1' type="password" placeholder='Digite sua senha' />
+				</div>
+
+				<div className='d-flex flex-column mt-3'>
+					<label htmlFor="">Confirmar senha</label>
+					<input className='mt-1' type="password" placeholder='Digite sua senha novamente' />
+				</div>
+
+				<button className='mt-3' type='submit'>Cadastrar e continuar</button>
+				<p className='mt-3 mx-auto'>Ainda não tem uma conta? <Link  onClick={e => setRegistrar(true)}>Crie uma conta.</Link> </p>
+
+			</div>
+		</form>
+		)}
+		
+		</>
 	)
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,23 @@
 import { Container, Row, Col } from 'reactstrap';
 import FormAccount from './FormAccount';
+import '../styles/global.scss'
+import '../styles/home.scss'
 
 export default function Home() {
 	return (
-		<Container>
-			<Row>
-				<Col md='8'>
-					<FormAccount />
-				</Col>
-				<Col md='4'>
-					<div>Olá</div>
-					<div>Olá</div>
-					<div>Olá</div>
-				</Col>
-			</Row>
-		</Container>
+		<div className="display">
+			<Container className='mt-4'>
+				<Row>
+					<Col md='8'>
+						<FormAccount />
+					</Col>
+					<Col md='4'>
+						<div>Olá</div>
+						<div>Olá</div>
+						<div>Olá</div>
+					</Col>
+				</Row>
+			</Container>
+		</div>
 	)
 }
