c97a824cbf4cc635fca936528aa862c55f6d8b77
vitorlombardi <83724421+vitorlombardi@users.noreply.github.com>
2021-10-01 19:12:47 -0300

    Update resumo.jsx

--- src/components/resumo.jsx
+++ src/components/resumo.jsx
@@ -1,13 +1,19 @@
+import React from 'react'
 import '../styles/resumo.scss'
 
 export default function Resumo(){
     return(
-        <div className='resumo'>
+        <div className='mr-3 resumo'>
             <h3 className='fw-bold'>Resumo da compra</h3>
-            <ul class="list-group mt-5">
+            {/* <ul class="list-group mt-5">
                 <li class="list-group-item d-flex justify-content-center"><b>dados da compra</b></li>
                 <li class="list-group-item">
                     <span><b>produtos comprados:</b></span>
+                    <div className='d-flex flex-row '>
+                        <span>Pneu Continental aro '16  * 205/55R16 91w run flat</span>
+                        <span>x</span>
+                        <span>R$350,00</span>
+                    </div>
                     <div className='d-flex flex-row '>
                         <span>Pneu Continental aro '16  * 205/55R16 91w run flat</span>
                         <span>x</span>
@@ -39,7 +45,7 @@ export default function Resumo(){
                     <div class="d-flex flex-column">
                         <span>(Crédito) Visa ****9999</span>
                         <span>Nome: Jorginho Silva</span>
-                        <span>vencimento:xx/xxxx</span>
+                        <span>vencimento: xx/xxxx</span>
                     </div>
                     
                 </li>
@@ -47,9 +53,57 @@ export default function Resumo(){
                     <span><b>Valor total:</b></span>
                     <span>R$1.103,90</span>
                 </li>
-            </ul>
+            </ul> */}
+            <div className='info mt-1 endereco'>
+                <span><b>Entraga no meu endereço</b></span>
+                <div className="d-flex flex-column">
+                    <span>Estrada Joaquim Cardoso Filho 3250</span>
+                    <span>Jardim São Marcos</span>
+                    <span>Itapecerica da Serra - SP</span>
+                    <span>CEP: 06872200</span>
+                </div>
 
-            <button className="mt-5">Confirmar compra</button>
+                <div>
+                    <span><b>Instalação em domicílio:</b></span>
+                    <span>
+                        Combo Básico 1 ou 2 Pneus 
+                        (Aro 12 - 16) no dia 30/09 com preferência 
+                        para o período da tarde.
+                    </span>
+                </div>
+
+            </div>
+           
+            <div className='info mt-1 pagamento'>
+                <span><b>detales do pagamento</b></span>
+                <div className="d-flex flex-column">
+                    <span>(Crédito) Visa ****9999</span>
+                    <span>Nome: Jorginho Silva</span>
+                    <span>vencimento: xx/xxxx</span>
+                </div>
+            </div>
+
+            <div className='info mt-1 itens'>
+                <span><b>produtos comprados:</b></span>
+                <div className='d-flex flex-row '>
+                    <span>Pneu Continental aro '16  * 205/55R16 91w run flat</span>
+                    <span>x</span>
+                    <span>R$350,00</span>
+                </div>
+                <div className='d-flex flex-row '>
+                    <span>Pneu Continental aro '16  * 205/55R16 91w run flat</span>
+                    <span>x</span>
+                    <span>R$350,00</span>
+                </div>
+            </div>
+
+            <div className='info mt-1 valor'>
+                <span><b>Valor total:</b></span>
+                <span>R$1.103,90</span>
+                <button className="button-confirma">Confirmar compra</button>
+            </div>
+
+            
         </div>
     )
 }
